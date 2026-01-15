c56aae69708f76e267af9fb0de35f25c806c874e
WenYang <wendeyang2@gmail.com>
2026-01-15 18:07:56 +0800

    push Cs_450_timer

--- HDLBits-Practice/Cs450_timer.sv
+++ HDLBits-Practice/Cs450_timer.sv
@@ -1,4 +1,23 @@
-module Cs450_timer();
+`timescale 1ns/1ps
+
+module top_module(
+    input clk,
+    input load,
+    input [9:0] data,
+    output tc
+  );
+
+  logic [9:0] tmp;
+
+  always_ff @(posedge clk)
+  begin
+    if(load)
+      tmp <= data;
+    else if(tmp > 0)
+      tmp <= tmp-1;
+    else if(tmp == 0)
+      tc <= 1;
+  end
 
 
 endmodule
