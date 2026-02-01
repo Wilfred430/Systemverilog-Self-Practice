38c012d07b8f56d924c20e93f7bb85c581c5aeb3
WenYang <wendeyang2@gmail.com>
2026-02-01 16:00:46 +0800

    add Rule90.sv

---
+++ HDLBits-Practice/Rule90.sv
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q );
+
+  always @(posedge clk)
+  begin
+    if(load)
+      q<=data;
+    else
+    begin
+      for(integer i=0;i<512;i=i+1)
+      begin
+        if(i == 0)
+          q[0] <= q[1];
+        else if(i == 511)
+          q[511] <= q[510];
+        else
+          q[i] <= q[i+1] ^ q[i-1];
+      end
+    end
+  end
+
+endmodule
